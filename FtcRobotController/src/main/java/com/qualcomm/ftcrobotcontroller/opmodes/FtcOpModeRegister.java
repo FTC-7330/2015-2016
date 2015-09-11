7d324823bf07d0aa864dab430649dd0f5bc64cbb
ElizabethRiffle <elizabeth@wa-inc.com>
2015-09-11 15:42:50 -0700

    Added Test Gyro

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -97,6 +97,8 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("PushBotDriveTouch", PushBotDriveTouch.class);
     manager.register("PushBotIrSeek", PushBotIrSeek.class);
     manager.register("PushBotSquare", PushBotSquare.class);
+    manager.register("TestGyro", TestGryo.class);
+
 
   }
 }
